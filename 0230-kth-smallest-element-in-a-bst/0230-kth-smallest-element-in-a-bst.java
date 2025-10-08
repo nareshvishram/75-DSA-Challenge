0bae057af4d668a3ac75bc1d4bac58ae878900ba
Naresh Vishram <58872658+nareshvishram@users.noreply.github.com>
2025-10-09 01:34:52 +0530

    Time: 0 ms (100%), Space: 45.1 MB (5.32%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,28 +14,22 @@
  * }
  */
 class Solution {
-    int ans=-1;
+    int ans = -1;
     int k;
+
     public int kthSmallest(TreeNode root, int k) {
-        // if(root==null)
-        //     return -1;
-        // kthSmallest(root.left,k-1);
-        // if(k==0)
-        //     return root.val;
-        // kthSmallest(root.right,k-1);
-        this.k=k;
+        this.k = k;
         helper(root);
         return ans;
-        
-        
     }
-    private void helper(TreeNode root){
-        if(root==null||k<0)
-            return ;
+
+    private void helper(TreeNode root) {
+        if (root == null || k < 0)
+            return;
         helper(root.left);
         k--;
-        if(k==0)
-            ans=root.val;
+        if (k == 0)
+            ans = root.val;
         helper(root.right);
     }
 }
