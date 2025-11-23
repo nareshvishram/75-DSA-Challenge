9c5f1ca5a7f2fb86d0a200c180a1a0d53bc8d787
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2025-11-23 23:27:11 +0530

    Time: 2 ms (2.46%), Space: 44.9 MB (7.79%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,21 @@
+class Solution {
+    int dp[][] = new int[101][101];
+
+    public int uniquePaths(int m, int n) {
+        for (int x[] : dp)
+            Arrays.fill(x, -1);
+        return dfs(0, 0, new boolean[m][n], m, n);
+    }
+
+    private int dfs(int i, int j, boolean[][] vis, int m, int n) {
+        if (i < 0 || j < 0 || i >= m || j >= n)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (i == m - 1 && j == n - 1)
+            return 1;
+        vis[i][j] = true;
+        dp[i][j] = dfs(i + 1, j, vis, m, n) + dfs(i, j + 1, vis, m, n);
+        return dp[i][j];
+    }
+}
