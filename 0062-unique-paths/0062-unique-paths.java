e002cc41b098b8f0348378637b2d9cff96bd0ecb
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2025-11-23 23:33:35 +0530

    Time: 1 ms (9.17%), Space: 44.9 MB (7.79%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -2,9 +2,10 @@ class Solution {
     int dp[][] = new int[101][101];
 
     public int uniquePaths(int m, int n) {
-        for (int x[] : dp)
-            Arrays.fill(x, -1);
-        return dfs(0, 0, new boolean[m][n], m, n);
+        // for (int x[] : dp)
+        //     Arrays.fill(x, -1);
+        // return dfs(0, 0, new boolean[m][n], m, n);
+        return tabulation(m,n);
     }
 
     private int dfs(int i, int j, boolean[][] vis, int m, int n) {
@@ -17,5 +18,20 @@ class Solution {
         vis[i][j] = true;
         dp[i][j] = dfs(i + 1, j, vis, m, n) + dfs(i, j + 1, vis, m, n);
         return dp[i][j];
+    }
+
+    private int tabulation(int m,int n){
+        int t[][]=new int[m][n];
+        for(int i=0;i<n;i++)
+            t[0][i]=1;
+        for(int j=0;j<m;j++)
+            t[j][0]=1;
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                t[i][j]=t[i-1][j]+t[i][j-1];
+            }
+        }
+        return t[m-1][n-1];
     }
 }
