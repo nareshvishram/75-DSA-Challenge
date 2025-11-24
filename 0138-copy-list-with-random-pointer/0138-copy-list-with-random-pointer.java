ea009f81017b9a11a21c90f45925d2b92267f2ee
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2025-11-24 23:19:51 +0530

    Time: 0 ms (100%), Space: 46.5 MB (21.87%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,43 +15,45 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Node ans=null;
-        ans=insert(head);
-        ans=link(head);
+        Node ans = null;
+        ans = insert(head);
+        ans = link(head);
         return removeLink(head);
     }
-    private Node insert(Node root){
-        Node ptr=root;
-        while(ptr!=null){
-            Node copy=new Node(ptr.val);
-            Node next=ptr.next;
-            ptr.next=copy;
-            copy.next=next;
-            ptr=next;
+
+    private Node insert(Node root) {
+        Node ptr = root;
+        while (ptr != null) {
+            Node copy = new Node(ptr.val);
+            Node next = ptr.next;
+            ptr.next = copy;
+            copy.next = next;
+            ptr = next;
         }
         return root;
     }
-    private Node link(Node root){
-        Node ptr=root;
-        while(ptr!=null){
-            Node copy=ptr.next;
-            if(ptr.random!=null)
-                copy.random=ptr.random.next;
+
+    private Node link(Node root) {
+        Node ptr = root;
+        while (ptr != null) {
+            if (ptr.random != null)
+                ptr.next.random = ptr.random.next;
             else
-                copy.random=null;
-            ptr=copy.next;
+                ptr.next.random = null;
+            ptr = ptr.next.next;
         }
         return root;
     }
-    private Node removeLink(Node root){
-        Node ptr=root;
-        Node dummy=new Node(-1);
-        Node res=dummy;
-        while(ptr!=null){
-            Node copy=ptr.next;
+
+    private Node removeLink(Node root) {
+        Node ptr = root;
+        Node dummy = new Node(-1);
+        Node res = dummy;
+        while (ptr != null) {
+            Node copy = ptr.next;
+            ptr.next=copy.next;
             res.next=copy;
             res=res.next;
-            ptr.next=copy.next;
             ptr=ptr.next;
         }
         return dummy.next;
