1bc3aeee417098936d5efb71fbc00feb05f6ce38
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2025-11-25 21:05:24 +0530

    Time: 10 ms (1.66%), Space: 44 MB (5.94%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,33 +1,40 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
+        int l = 0, r = n - 1;
         if (n == 1)
             return nums[0] == target ? 0 : -1;
-        int pivot = -1, l = 0, r = n - 1;
+        //1. find pivot
+        //2. find range
+        //3. apply binary search
+
+        //1. find pivot
+        int pivot = -1;
         while (l <= r) {
             int mid = (r - l) / 2 + l;
-            if(nums[mid]==target)
+            if (nums[mid] == target)
                 return mid;
             if (nums[mid] >= nums[0])
                 l = mid + 1;
             else
                 r = mid - 1;
         }
-        //System.out.println(l + " " + r);
         pivot = l;
+        //2. find range
         if (target >= nums[0])
             return bs(0, pivot - 1, nums, target);
-        else
-            return bs(pivot, n - 1, nums, target);
-        //return l;
+
+        return bs(pivot, n - 1, nums, target);
+
     }
 
-    private int bs(int l, int r, int[] nums, int ele) {
+    private int bs(int l, int r, int nums[], int target) {
         while (l <= r) {
             int mid = (r - l) / 2 + l;
-            if (nums[mid] == ele)
+            System.out.println(l+" "+r+" "+mid);
+            if (nums[mid] == target)
                 return mid;
-            if (nums[mid] > ele)
+            if (nums[mid] > target)
                 r = mid - 1;
             else
                 l = mid + 1;
