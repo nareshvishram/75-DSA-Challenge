2d957494c400c11c79128c847358853e4cdc3bd6
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2025-11-27 00:26:59 +0530

    Time: 8 ms (20.88%), Space: 45.9 MB (39.72%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -18,8 +18,7 @@ class Solution {
     private boolean helper(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null)
             return false;
-        boolean mid = (root.val == p.val || root.val == q.val);
-
+        boolean mid = (root == p) || (root == q);
         boolean left = helper(root.left, p, q);
         boolean right = helper(root.right, p, q);
         int eval = (left ? 1 : 0) + (right ? 1 : 0) + (mid ? 1 : 0);
