eb2b76be342026b35f5a29c363bf82b3e1d612e6
Naresh Kumar Vishram <58872658+nareshvishram@users.noreply.github.com>
2025-11-28 01:52:22 +0530

    Time: 2 ms (38.04%), Space: 45.3 MB (31.07%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,22 @@
+class Solution {
+    List<List<Integer>> res;
+
+    public List<List<Integer>> permute(int[] nums) {
+        res = new ArrayList<>();
+        helper(nums, new ArrayList<>());
+        return res;
+    }
+
+    private void helper(int nums[], List<Integer> tmp) {
+        if (tmp.size() == nums.length) {
+            res.add(new ArrayList<>(tmp));
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (tmp.contains(nums[i]))
+                continue;
+            tmp.add(nums[i]);
+            helper(nums, tmp);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
